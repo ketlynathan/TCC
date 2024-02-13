e9aa40f51b12e9b4bb07fecea96f6abde0514093
Kgoncalves <kgoncalves@lear.com>
2024-02-12 22:24:51 -0300

    feat: create call main

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from loginStrava import fazer_login_strava
+from formatData import limpar_e_converter_dataframe
+from InsertData import inserir_dataframe_no_banco
 from loguru import logger
 
-logger.info("Started call Login")
 if __name__ == "__main__":
-    # Realiza o login e o scraping das atividades
-    fazer_login_strava()
+    logger.info("Started call Login")
+    
+    try:
+        # Realiza o login e o scraping das atividades
+        dataframe_sport = fazer_login_strava()
+        
+
+        # Verifica se o DataFrame não está vazio
+        if not dataframe_sport.empty:
+            dataframe_limpo = limpar_e_converter_dataframe(dataframe_sport)
+            logger.debug("Yes content, data transform")
+           
+            # Insere o DataFrame limpo no banco de dados
+            inserir_dataframe_no_banco(dataframe_limpo)
+
+        else:
+            logger.error("The DataFrame is empty.")
+
+    except Exception as e:
+        logger.error(f"An error occurred during the process: {str(e)}")
