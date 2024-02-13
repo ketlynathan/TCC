f349e6e64a7407c3c52678ee598fc04d17df1c5f
Kgoncalves <kgoncalves@lear.com>
2024-02-12 22:23:27 -0300

    fix: call the function

--- loginStrava.py
+++ loginStrava.py
@@ -7,63 +7,58 @@ import pandas as pd
 
 
 def fazer_login_strava():
-    timeStart = time.time()
-    
+    # Tempo de início
+    time_start = time.time()
 
-    # Chama a função para obter as credenciais
+    # Obter credenciais
     credenciais, conexao = obter_credenciais()
-    logger.debug("Starting access to the website")
+    logger.debug("Iniciando acesso ao site")
 
-    # Verifica se as credenciais foram obtidas com sucesso
     if credenciais:
         email, password = credenciais[0]
-        logger.info("Validated email and password")  # Acesse o primeiro elemento da lista de credenciais
+        logger.info("E-mail e senha validados")
     else:
-        logger.error("Unable to obtain credentials.")
+        logger.error("Não foi possível obter as credenciais.")
 
-    # Verificar se as credenciais foram obtidas com sucesso
+    # Iniciar navegador
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        time.sleep(5)
+        browser = p.chromium.launch()
         page = browser.new_page()
         page.goto("https://www.strava.com/login")
-        page.locator('xpath=//*[@id="email"]').click()
         page.fill('xpath=//*[@id="email"]', email)
-        time.sleep(3)
-        page.locator('xpath=//*[@id="password"]').click()
         page.fill('xpath=//*[@id="password"]', password)
-        time.sleep(3)
-        page.locator('xpath=//*[@id="login-button"]').click()
+        page.click('xpath=//*[@id="login-button"]')
         logger.info("Login realizado")
-        finalTime = time.time()
-        logger.info(f"Login done at {int(finalTime - timeStart)} seconds")
 
-        logger.info("Inicando o Scraping")
-        timeInicioScrap = time.time()
+        # Scraping das atividades
+        logger.info("Iniciando o scraping")
         page.goto("https://www.strava.com/athlete/training")
-        time.sleep(3)
-        table_html = page.locator('xpath=//*[@id="search-results"]').inner_html()
-        logger.debug("Table located on the page")
 
+        # Esperar até que a tabela de atividades seja carregada
+        page.wait_for_selector('xpath=//*[@id="search-results"]')
+
+        # Extrair HTML da tabela
+        table_html = page.inner_html('xpath=//*[@id="search-results"]')
         soup = BeautifulSoup(table_html, 'html.parser')
 
-        activitys = soup.find_all('tr', attrs={'class': 'training-activity-row'})
-        logger.debug("Captured table")
-
+        # Extrair dados das atividades
         lista_activitys = []
-
+        activitys = soup.find_all('tr', attrs={'class': 'training-activity-row'})
         for activity in activitys:
-            typeActivity = activity.find('td', attrs={'class': 'view-col col-type'})
-            dateActivity = activity.find('td', attrs={'class': 'view-col col-date'})
-            titleActivity = activity.find('td', attrs={'class': 'view-col col-title'})
-            timeActivity = activity.find('td', attrs={'class': 'view-col col-time'})
-            distActivity = activity.find('td', attrs={'class': 'view-col col-dist'})
-            elevActivity = activity.find('td', attrs={'class': 'view-col col-elev'})
-            
-            lista_activitys.append([typeActivity.text, dateActivity.text, titleActivity.text, timeActivity.text, distActivity.text, elevActivity.text])
+            typeActivity = activity.find('td', attrs={'class': 'view-col col-type'}).text
+            dateActivity = activity.find('td', attrs={'class': 'view-col col-date'}).text
+            titleActivity = activity.find('td', attrs={'class': 'view-col col-title'}).text
+            timeActivity = activity.find('td', attrs={'class': 'view-col col-time'}).text
+            distActivity = activity.find('td', attrs={'class': 'view-col col-dist'}).text
+            elevActivity = activity.find('td', attrs={'class': 'view-col col-elev'}).text
+            lista_activitys.append([typeActivity, dateActivity, titleActivity, timeActivity, distActivity, elevActivity])
 
+        # Criar DataFrame
         sport = pd.DataFrame(lista_activitys, columns=['Tipo', 'Data', 'Title', 'Tempo', 'Distancia', 'Elevacao'])
-        logger.debug("Add array in Data Frame send to excel")
-        sport.to_excel('Activitys.xlsx', index=False)
-        timeFinalScrap = time.time()
-        logger.info(f'Scraping done in {int(timeFinalScrap - timeInicioScrap)} seconds')
+
+        # Tempo final
+        time_end = time.time()
+        logger.info(f'Scraping concluído em {int(time_end - time_start)} segundos')
+
+    return sport
+
