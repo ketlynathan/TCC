f95ae626685332b610e02c78ce1f3e543608c801
Kgoncalves <kgoncalves@lear.com>
2024-02-14 19:11:03 -0300

    feat: add test of connect

---
+++ testConect.py
@@ -0,0 +1,21 @@
+import unittest
+from connectDB import BancoDados
+from getCredential import ObterCredenciais
+
+class TestBancoDados(unittest.TestCase):
+    def TestConexaoDados(self):
+        # Teste se a função BancoDados retorna uma conexão válida
+        conexao = BancoDados()
+        self.assertIsNotNone(conexao)
+        conexao.close()  # Feche a conexão após o teste
+
+class TestObterCredenciais(unittest.TestCase):
+    def testObterCredenciais(self):
+        # Teste se a função obter_credenciais retorna uma lista de credenciais não vazia
+        credenciais, conexao = ObterCredenciais()
+        self.assertIsNotNone(credenciais)
+        self.assertIsNotNone(conexao)
+        conexao.close()  # Feche a conexão após o teste
+
+if __name__ == '__main__':
+    unittest.main()
